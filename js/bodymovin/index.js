b8e5e60e3c43382c2a224cb28a470e937b5eeb5f
nonxodesign <info@nonxo.com>
2019-03-10 02:24:28 +0800

    mobile_js v2

---
+++ js/bodymovin/index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('bm'),
+	render:'svg',
+	loop: true,
+	autoplay: true,
+	path: './js/bodymovin/data.json'
+})
--- js/xo2.js
+++ js/xo2.js
@@ -4,12 +4,15 @@ var toggleStatus = 1;
 			if (toggleStatus == 1) {
 				document.getElementById("mobie_menu").style.top = "0";
 				document.getElementById("mobie_menu").style.opacity = "1";
+				$(".menu_button1").transition({ opacity:1, y:0}, 2500 ,'ease');
 				$(".grid_project").fadeOut("ease");
 				toggleStatus = 0;
+
 			} else if (toggleStatus == 0) {
 				document.getElementById("mobie_menu").style.top = "100vh";
 				document.getElementById("mobie_menu").style.opacity = "0";
 				$(".grid_project").fadeIn("ease");
+				$(".menu_button1").transition({ opacity:0 },500 ,'ease');
 				toggleStatus = 1;
 			}
 		}
