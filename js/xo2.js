f27c34b9e49aa9d6dc38d51a625f6dad91149dc4
nonxodesign <info@nonxo.com>
2019-03-10 17:11:59 +0800

    mobile_5

--- js/xo2.js
+++ js/xo2.js
@@ -5,13 +5,13 @@ var toggleStatus = 1;
 				document.getElementById("mobie_menu").style.top = '18vh';
 				document.getElementById("mobie_menu").style.opacity = "1";
 				$(".menu_button1").transition({ opacity:1, y:0}, 2500 ,'ease');
-				$(".grid_project").fadeOut("slow");
+				$(".grid_project").transition({ opacity:0 }, 1000, 'ease');
 				toggleStatus = 0;
 
 			} else if (toggleStatus == 0) {
 				document.getElementById("mobie_menu").style.top = "100vh";
 				document.getElementById("mobie_menu").style.opacity = "0";
-				$(".grid_project").fadeIn("ease");
+				$(".grid_project").transition({ opacity:1 }, 1500, 'ease');
 				$(".menu_button1").transition({ opacity:0 },500 ,'ease');
 				toggleStatus = 1;
 			}
