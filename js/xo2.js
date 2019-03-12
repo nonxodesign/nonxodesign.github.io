87d1152e853a34f70ff2528eb0125bb1599cfe6f
nonxodesign <info@nonxo.com>
2019-03-13 03:56:59 +0800

    mobile_finished2

--- js/xo2.js
+++ js/xo2.js
@@ -1,16 +1,16 @@
 
-// var toggleStatus = 1;
-// 		function mobile_menu_iconbox() {
-// 			if (toggleStatus == 1) {
-// 				document.getElementById("mobie_menu").style.top = '18vh';
-// 				document.getElementById("mobie_menu").style.opacity = "1";
-// 				$(".grid_project").transition({ opacity:.01 }, 500, 'ease');
-// 				toggleStatus = 0;
+var toggleStatus = 1;
+		function mobile_menu_iconbox() {
+			if (toggleStatus == 1) {
+				document.getElementById("mobie_menu").style.top = '18vh';
+				document.getElementById("mobie_menu").style.opacity = "1";
+				$(".grid_project").transition({ opacity:.01 }, 500, 'ease');
+				toggleStatus = 0;
 
-// 			} else if (toggleStatus == 0) {
-// 				document.getElementById("mobie_menu").style.top = "100vh";
-// 				document.getElementById("mobie_menu").style.opacity = "0";
-// 				$(".grid_project").transition({ opacity:1 }, 1500, 'ease');
-// 				toggleStatus = 1;
-// 			}
-// 		}
+			} else if (toggleStatus == 0) {
+				document.getElementById("mobie_menu").style.top = "100vh";
+				document.getElementById("mobie_menu").style.opacity = "0";
+				$(".grid_project").transition({ opacity:1 }, 1500, 'ease');
+				toggleStatus = 1;
+			}
+		}
