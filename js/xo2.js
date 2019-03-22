8cb1642ccad7b19064c119bf0b3b8966df9a3911
nonxodesign <info@nonxo.com>
2019-03-22 13:16:19 +0800

    temp_v1

--- js/xo2.js
+++ js/xo2.js
@@ -2,15 +2,15 @@
 var toggleStatus = 1;
 		function mobile_menu_iconbox() {
 			if (toggleStatus == 1) {
-				document.getElementById("mobie_menu").style.top = '18vh';
+				document.getElementById("mobie_menu").style.top = '22vh';
 				document.getElementById("mobie_menu").style.opacity = "1";
-				$(".grid_project").transition({ opacity:.01 }, 500, 'ease');
+				$(".grid_project").transition({ opacity:.01 }, 500, 'ease-out');
 				toggleStatus = 0;
 
 			} else if (toggleStatus == 0) {
 				document.getElementById("mobie_menu").style.top = "100vh";
 				document.getElementById("mobie_menu").style.opacity = "0";
-				$(".grid_project").transition({ opacity:1 }, 1500, 'ease');
+				$(".grid_project").transition({ opacity:1 }, 1500, 'ease-out');
 				toggleStatus = 1;
 			}
 		}
@@ -20,15 +20,25 @@ var toggleStatus = 1;
 		function about__link() {
 			if (toggleStatus == 1) {
 				document.getElementById("about_show").style.top = '0vh';
+
 				$(".grid_project").transition({ opacity:.01 }, 500, 'ease');
+				$(".about__link").transition({ y:'-45.5vh' }, 1500, 'ease');
+				$('.about_cross').transition({ opacity:.5 },800 ,'ease');
 				toggleStatus = 0;
 
+
 			} else if (toggleStatus == 0) {
+
 				document.getElementById("about_show").style.top = "100vh";
 				$(".grid_project").transition({ opacity:1 }, 1500, 'ease');
+				$(".about__link").transition({ y:'0' }, 1000, 'ease');
+				$('.about_cross').transition({ opacity:0 },800 ,'ease');
 				toggleStatus = 1;
-			}
-		}
+			
+
+		}}
+
+
 
 
 
