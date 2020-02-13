6bd69cd7b4e4721edc644e4c8f4be4d3afd298d8
nonxodesign <info@nonxo.com>
2020-02-14 02:37:14 +0800

    renew & 藝穗節

--- js/xo2.js
+++ js/xo2.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
 	});
 	$('#return-to-top').click(function() {      // When arrow is clicked
 	    $('body,html').animate({
-	        scrollTop : 0                       // Scroll to top of body
+	        scrollTop : 50                       // Scroll to top of body
 	    }, 500);
 	});
 });
@@ -73,3 +73,6 @@ $(document).ready(function(){
                 });
 
             });
+
+
+                
